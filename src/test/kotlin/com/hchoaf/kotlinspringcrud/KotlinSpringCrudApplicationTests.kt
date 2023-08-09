d3485e02b54d674ff4d45bce1b23f7ba43456164
Hangsun Cho <hchoaf@connect.ust.hk>
2023-08-09 22:19:26 +0800

    initial commit

---
+++ src/test/kotlin/com/hchoaf/kotlinspringcrud/KotlinSpringCrudApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hchoaf.kotlinspringcrud
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringCrudApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
